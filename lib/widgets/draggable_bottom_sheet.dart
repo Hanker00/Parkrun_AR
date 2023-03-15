e660ac3bf2f1c125ecd951bd4d29f08cd6ad2275
Axel Söderberg <karlaxel01@hotmail.com>
2023-03-15 17:08:05 +0100

    fix: wrapped DBS with a column

--- lib/widgets/draggable_bottom_sheet.dart
+++ lib/widgets/draggable_bottom_sheet.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:parkrun_ar/widgets/top_progress_info.dart';
 
-class DraggableBottomSheet extends StatelessWidget {
+class DraggableBottomSheet extends StatefulWidget {
   const DraggableBottomSheet({super.key});
 
+  @override
+  State<DraggableBottomSheet> createState() => _DraggableBottomSheetState();
+}
+
+class _DraggableBottomSheetState extends State<DraggableBottomSheet> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,37 +21,42 @@ class DraggableBottomSheet extends StatelessWidget {
           return SingleChildScrollView(
             controller: scrollController,
             // This container adds the coloured bar on top
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-              ),
-              // This column holds the actual content
-              child: Column(
-                children: [
-                  const SizedBox(height: 4),
-                  // The row contains the small "scrollable" indicator
-                  Row(
+            child: Column(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  // This column holds the actual content
+                  child: Column(
                     children: [
-                      const Spacer(),
-                      Container(
-                        height: 8,
-                        width: 70,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15.0),
-                          color: Colors.amberAccent,
-                        ),
+                      const SizedBox(height: 4),
+                      // This row contains the small "scrollable" indicator
+                      Row(
+                        children: [
+                          const Spacer(),
+                          Container(
+                            height: 8,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15.0),
+                              color: Colors.amberAccent,
+                            ),
+                          ),
+                          const Spacer(),
+                        ],
                       ),
-                      const Spacer(),
+                      const SizedBox(height: 4),
                     ],
                   ),
-                  const SizedBox(height: 4),
-                ],
-                // The sheet content goes here!
-              ),
+                ),
+                TopProgressInfo(),
+                // Additional Widgets go here
+              ],
             ),
           );
         },
