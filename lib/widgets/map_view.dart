fa715458161ffeb67d55b78862058ba8ffac5960
ClaraSimonsson <clara.simonsson@hotmail.se>
2023-03-16 16:02:54 +0100

    feature: see users current location

--- lib/widgets/map_view.dart
+++ lib/widgets/map_view.dart
@@ -1,8 +1,12 @@
+import "dart:async";
+
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
 import "../constants.dart";
 import 'package:latlong2/latlong.dart';
+//import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import "../models/map_markers/map_marker.dart";
+import 'package:geolocator/geolocator.dart';
 
 class MapView extends StatefulWidget {
   final double startLongitude;
@@ -20,6 +24,11 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
+  MapController _mapController = MapController();
+  late Position position;
+  double long = 0, lat = 0;
+  late StreamSubscription<Position> positionStream;
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
@@ -29,6 +38,7 @@ class _MapViewState extends State<MapView> {
         zoom: 13,
         center: LatLng(widget.startLatitude, widget.startLongitude),
       ),
+      mapController: _mapController,
       layers: [
         TileLayerOptions(
           urlTemplate:
@@ -38,6 +48,9 @@ class _MapViewState extends State<MapView> {
             'accessToken': AppConstants.mapBoxAccessToken,
           },
         ),
+        /* LocationMarkerLayerOptions(
+
+        ), */
         MarkerLayerOptions(
           markers: [
             for (int i = 0; i < widget.mapMarkers.length; i++)
@@ -56,5 +69,33 @@ class _MapViewState extends State<MapView> {
         ),
       ],
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  getCurrentLocation() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    LocationSettings locationSettings = const LocationSettings(
+      accuracy: LocationAccuracy.high, //accuracy of the location data
+      distanceFilter: 100, //minimum distance (measured in meters) a
+      //device must move horizontally before an update event is generated;
+    );
+
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position position) {
+      long = position.longitude;
+      lat = position.latitude;
+
+      setState(() {
+        //_center = LatLng(_locationData.latitude, _locationData.longitude);
+        _mapController.move(LatLng(lat, long), 13.0);
+      });
+    });
   }
 }
