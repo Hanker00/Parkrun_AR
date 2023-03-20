5db051b3761951383e088a9c61edad195eb1c813
eilinn <elinmelander@gmail.com>
2023-03-20 10:42:47 +0100

    themepage setup

---
+++ lib/themeData/theme.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+const Color colorPrimary = Color(0xFFFFA300);
+const Color colorPrimVar = Color(0xFFffe768);
+const Color colorSecondary = Color(0xFF54bfb5);
+Theme(
+  data: ThemeData( 
+  primaryColor: colorPrimary,)
+);
