a2e2447283b801e23a1e73f8a77c08f11939d2c0
eilinn <elinmelander@gmail.com>
2023-03-20 11:07:15 +0100

    theme updated

--- lib/themeData/theme.dart
+++ lib/themeData/theme.dart
@@ -1,10 +1,23 @@
-import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 const Color colorPrimary = Color(0xFFFFA300);
-const Color colorPrimVar = Color(0xFFffe768);
+const Color colorPrimaryLight = Color(0xFFffe768);
 const Color colorSecondary = Color(0xFF54bfb5);
-Theme(
-  data: ThemeData( 
-  primaryColor: colorPrimary,)
+
+ThemeData mainTheme = ThemeData(
+  primaryColor: colorPrimary,
+  
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+        EdgeInsets.symmetric(horizontal: 40.0,vertical: 20.0)
+      ),
+      shape: MaterialStateProperty.all<OutlinedBorder>(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0))
+      ),
+      backgroundColor: MaterialStateProperty.all<Color>(colorPrimaryLight)
+    )
+  ),
 );
