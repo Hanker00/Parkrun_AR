cf8f217e64357ec72d99d127fe82578d9bdf56f3
Axel Söderberg <karlaxel01@hotmail.com>
2023-03-22 12:40:41 +0100

    feat: added start button

--- lib/widgets/top_progress_info.dart
+++ lib/widgets/top_progress_info.dart
@@ -15,17 +15,24 @@ class _TopProgressInfoState extends State<TopProgressInfo> with ChangeNotifier {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: const [
-        Padding(padding: EdgeInsets.only(left: 18, top: 35, bottom: 35)),
-        Text(
+      children: [
+        const Padding(padding: EdgeInsets.only(left: 18, top: 35, bottom: 35)),
+        const Text(
           "T min ",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
         ),
-        Text(
+        const Text(
           "(Dist m)",
           style: TextStyle(color: Colors.grey, fontSize: 28),
         ),
-        Spacer()
+        const Spacer(),
+        ElevatedButton(
+          onPressed: () {},
+          style: Theme.of(context).elevatedButtonTheme.style,
+          child: const Text(
+            "Start",
+          ),
+        ),
       ],
     );
   }
