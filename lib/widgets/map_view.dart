eedf4f3b74a6cf3eaacbdbfea9434bda88d05f2e
Hamid <113691788+HamidHosseini313@users.noreply.github.com>
2023-03-22 17:56:44 +0100

    infobox
    ändrat namnen och tagit bort extra widgets

--- lib/widgets/map_view.dart
+++ lib/widgets/map_view.dart
@@ -5,7 +5,7 @@ import "package:parkrun_ar/services/polyline_service.dart";
 import "../constants.dart";
 import 'package:latlong2/latlong.dart';
 import "../models/map_markers/map_marker.dart";
-import "marker_inobox.dart";
+import "markers_infobox.dart";
 
 class MapView extends StatefulWidget {
   final double startLongitude;
