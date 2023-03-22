6003bf2583f58755a71c19b203ed11c8d97e65b6
eilinn <elinmelander@gmail.com>
2023-03-22 13:51:40 +0100

    buttons in stepper

--- lib/widgets/all_stepper.dart
+++ lib/widgets/all_stepper.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:parkrun_ar/models/map_markers/map_marker.dart';
 import 'package:enhance_stepper/enhance_stepper.dart';
 
@@ -20,16 +18,14 @@ class _AllStepperState extends State<AllStepper> {
   _AllStepperState();
 
   void go(int index) {
-    if (index == -1 && _index <= 0 ) {
+    if (index == -1 && _index <= 0) {
       print("it's first Step!");
       return;
     }
-
     if (index == 1 && _index >= widget.mapMarkers.length - 1) {
       print("it's last Step!");
       return;
     }
-
     setState(() {
       _index += index;
     });
@@ -38,62 +34,69 @@ class _AllStepperState extends State<AllStepper> {
   @override
   Widget build(BuildContext context) {
     return EnhanceStepper(
-        stepIconSize: 30,
-        type: _type,
-        horizontalTitlePosition: HorizontalTitlePosition.bottom,
-        horizontalLinePosition: HorizontalLinePosition.top,
-        currentStep: _index,
-        physics: ClampingScrollPhysics(),
-        steps: widget.mapMarkers.map((sign) => EnhanceStep(
-          icon: Icon(sign.markerIcon, color: Colors.blue, size: 30,),
-          isActive: _index == widget.mapMarkers.indexOf(sign),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(sign.title),
-              Checkbox(value: true,
-      onChanged: (bool? value) {
-        setState(() {
-          value = false;
-        });
-      },),
-            ],
-          ),
-          subtitle: Text(sign.description),
-          content: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text("250m"),
+      stepIconSize: 30,
+      type: _type,
+      horizontalTitlePosition: HorizontalTitlePosition.bottom,
+      horizontalLinePosition: HorizontalLinePosition.top,
+      currentStep: _index,
+      physics: ClampingScrollPhysics(),
+      steps: widget.mapMarkers
+          .map((sign) => EnhanceStep(
+              icon: Icon(
+                sign.markerIcon,
+                color: Colors.blue,
+                size: 30,
               ),
-            ],
-          )
-        )).toList(),
-        onStepCancel: () {
-          go(-1);
-        },
-        onStepContinue: () {
-          go(1);
-        },
-        onStepTapped: (index) {
-          print(index);
-          setState(() {
-            _index = index;
-          });
-        },
-        controlsBuilder: (BuildContext context, ControlsDetails controls) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      TextButton(onPressed: controls.onStepCancel, 
-                      child: const Text("CANCEL")),
-                      SizedBox(height: 30,),
-                      ElevatedButton(onPressed: controls.onStepContinue, 
-                      child: const Text("NEXT SIGN"))
-                    ]
-                  );
-                },
+              isActive: _index == widget.mapMarkers.indexOf(sign),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(sign.title),
+                  Checkbox(
+                    value: true,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        value = false;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              subtitle: Text(sign.description),
+              content: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("250m"),
+                  ),
+                ],
+              )))
+          .toList(),
+      onStepCancel: () {
+        go(-1);
+      },
+      onStepContinue: () {
+        go(1);
+      },
+      onStepTapped: (index) {
+        print(index);
+        setState(() {
+          _index = index;
+        });
+      },
+      controlsBuilder: (BuildContext context, ControlsDetails controls) {
+        return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton(
+                  onPressed: controls.onStepCancel,
+                  child: const Text("go back")),
+              ElevatedButton(
+                  onPressed: controls.onStepContinue,
+                  child: const Text("NEXT SIGN"))
+            ]);
+      },
     );
   }
 }
