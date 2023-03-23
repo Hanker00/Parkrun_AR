0e42d67c6ffb150a7ec52ef7609740266a608979
ClaraSimonsson <clara.simonsson@hotmail.se>
2023-03-23 16:18:25 +0100

    feature: Map is implemented and the user's location is showed

--- lib/screens/map_navigation.dart
+++ lib/screens/map_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:parkrun_ar/constants.dart';
 
 import '../models/shared_prefs.dart';
 
@@ -12,22 +13,50 @@ class MapNavigation extends StatefulWidget {
 
 class _MapNavigationState extends State<MapNavigation> {
   LatLng latLng = getLatLngFromSharedPrefs();
+  late CameraPosition _initialCameraPosition;
+  late MapboxMapController controller;
 
   @override
   void initState() {
     super.initState();
+    _initialCameraPosition = CameraPosition(target: latLng, zoom: 15);
   }
+
+  _onMapCreated(MapboxMapController controller) async {
+    this.controller = controller;
+  }
+
+  _onStyleLoadedCallback() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Restaurants Map'),
+        title: const Text('Map navigation test'),
       ),
       body: SafeArea(
-        child: Center(
-          child: Text('${latLng.latitude}, ${latLng.longitude}'),
+        child: Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              child: MapboxMap(
+                accessToken: AppConstants.mapBoxAccessToken,
+                initialCameraPosition: _initialCameraPosition,
+                onMapCreated: _onMapCreated,
+                onStyleLoadedCallback: _onStyleLoadedCallback,
+                myLocationEnabled: true,
+                myLocationTrackingMode: MyLocationTrackingMode.TrackingGPS,
+              ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          controller.animateCamera(
+              CameraUpdate.newCameraPosition(_initialCameraPosition));
+        },
+        child: const Icon(Icons.my_location),
       ),
     );
   }
