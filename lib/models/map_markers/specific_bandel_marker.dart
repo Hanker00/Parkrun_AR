50f38b77130716bf71d74909f0be9d609c373351
eilinn <elinmelander@gmail.com>
2023-03-29 15:49:47 +0200

    changes to titles in markers

--- lib/models/map_markers/specific_bandel_marker.dart
+++ lib/models/map_markers/specific_bandel_marker.dart
@@ -10,34 +10,34 @@ class bandel_marks {
   ];
 
   static final List<MapMarker> _bandel_1 = [
-    DirectionMarker("Rakt fram 3",
+    DirectionMarker("Rakt fram",
         "Skylt rakt fram strax efter fyrvägskorsningen", 57.70631, 12.04014),
     DirectionMarker.right(
-        "Höger 4",
+        "Höger",
         "Skylt vid stenen som leder deltagarna till höger, fortsatt på åttan.",
         57.70743,
         12.03822),
     KilometerMarker.one(
-        "1 km id:9",
+        "1 km",
         "Skylt 1 km under den sista högspänningsledningen.",
         57.70771,
         12.03938),
     KilometerMarker.two(
-        "2 km id:10",
+        "2 km",
         "Skylt 2 km vid klippväggen på vänster sida, strax innan staketet börjar.",
         57.71038,
         12.05371),
-    DirectionMarker("Rakt fram 5", "Skylt rakt fram i höjd med staketet",
+    DirectionMarker("Rakt fram", "Skylt rakt fram i höjd med staketet",
         57.7103, 12.05403),
-    DirectionMarker.right("Höger id:6",
+    DirectionMarker.right("Höger",
         "Skylt mot höger, framför brun fast skylt.", 57.71056, 12.05433),
     DirectionMarker.right(
-        "Höger id:7",
+        "Höger",
         "Skylt höger som leder deltagarna upp på Ormeslättsstigen, bakom Servicehuset.",
         57.71046,
         12.05523),
     DirectionMarker.right(
-        "Höger id:8",
+        "Höger",
         "Skylt höger som leder deltagarna vidare på Ormeslättsstigen, strax efter en liten backe.",
         57.70652,
         12.05289),
@@ -47,13 +47,13 @@ class bandel_marks {
 
   static final List<MapMarker> _bandel_2 = [
     KilometerMarker.three(
-      "3km",
+      "3 km",
       "",
       57.7043708,
       12.0472277,
     ),
     DirectionMarker.left(
-      "vänster",
+      "Vänster",
       "Skylt vänster som leder deltagarna upp på 2,5:an - Gröna stigen. Med fördel en skylt innan svängen och en efter.",
       57.7042189,
       12.0447332,
@@ -67,7 +67,7 @@ class bandel_marks {
   static List<MapMarker> get mapMarker_bandel_3 => _bandel_3;
 
   static final List<MapMarker> _bandel_3 = [
-    DirectionMarker("Rakt fram 3", "Vid husknuten", 57.7030581, 12.0370728),
+    DirectionMarker("Rakt fram", "Vid husknuten", 57.7030581, 12.0370728),
     DirectionMarker(
         "Rakt fram", "I backen efter korsningen", 57.6995955, 12.0370352),
     KilometerMarker.four("4 km", "", 57.6994292, 12.0395458),
