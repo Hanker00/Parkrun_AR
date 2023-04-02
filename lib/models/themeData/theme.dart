ab7a20b4e72447b88b429e6ff061cbbc4aed2cf2
eilinn <elinmelander@gmail.com>
2023-04-02 10:49:05 +0200

    update for theme page

--- lib/models/themeData/theme.dart
+++ lib/models/themeData/theme.dart
@@ -8,18 +8,24 @@ const Color colorSecondary = Color(0xFF54bfb5);
 class parkrunTheme {
   static ThemeData mainTheme() {
     return ThemeData(
+      //theme for colors in app
       primaryColor: colorPrimary,
       colorScheme: colors(Brightness.light, colorPrimary),
       appBarTheme: const AppBarTheme(),
       listTileTheme: const ListTileThemeData(),
+
+      //Theme for text
       textTheme: const TextTheme(
         displayLarge: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
         displayMedium: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         displaySmall: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
       ),
+
+      //styles for different widgets
       elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
         backgroundColor: colorPrimary,
+        //minimumSize: Size(20, 20) ,
         elevation: 6.0,
         shape:
             RoundedRectangleBorder(borderRadius: BorderRadius.circular(35.0)),
