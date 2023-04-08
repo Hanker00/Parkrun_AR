75b55e1ba1cf7e8897e014ea7f48bcbc9d0e4709
Sallishsh <sallishsh@gmail.com>
2023-04-08 20:26:51 +0200

    feature: added current position permision granter

---
+++ lib/widgets/current_position.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
+
+class CurrentPosition extends StatefulWidget {
+  CurrentPosition({super.key});
+
+  @override
+  State<CurrentPosition> createState() => _CurrentPosition();
+}
+
+class _CurrentPosition extends State<CurrentPosition> {
+  Future<LocationData?> _currentLocation() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    Location location = new Location();
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return null;
+      }
+    }
+    return await location.getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<LocationData?>(
+      future: _currentLocation(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapchat) {
+        if (snapchat.hasData) {
+          final LocationData currentLocation = snapchat.data;
+          return SfMaps(
+            layers: [
+              MapTileLayer(
+                initialFocalLatLng: MapLatLng(
+                    currentLocation.latitude!, currentLocation.longitude!),
+                initialZoomLevel: 5,
+                initialMarkersCount: 1,
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                markerBuilder: (BuildContext context, int index) {
+                  return MapMarker(
+                    latitude: currentLocation.latitude!,
+                    longitude: currentLocation.longitude!,
+                    child: Icon(
+                      Icons.location_on,
+                      color: Colors.red[800],
+                    ),
+                    size: Size(20, 20),
+                  );
+                },
+              ),
+            ],
+          );
+        }
+        return Center(child: CircularProgressIndicator());
+      },
+    );
+  }
+}
