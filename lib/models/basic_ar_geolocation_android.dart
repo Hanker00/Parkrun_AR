4905c61f186ee3e009fcf745163843f1292dce1f
Axel Söderberg <karlaxel01@hotmail.com>
2023-04-17 15:50:41 +0200

    feat: ar view withhout angle integration

--- lib/models/basic_ar_geolocation_android.dart
+++ lib/models/basic_ar_geolocation_android.dart
@@ -1,3 +1,4 @@
+import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
@@ -8,25 +9,42 @@ import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:geolocator/geolocator.dart';
 
-class HelloWorld extends StatefulWidget {
+class BasicArGeolocation extends StatefulWidget {
   @override
-  _HelloWorldState createState() => _HelloWorldState();
+  _BasicArGeolocationState createState() => _BasicArGeolocationState();
 }
 
 enum WidgetDistance { ready, navigating }
 
 enum WidgetCompass { scanning, directing }
 
-class _HelloWorldState extends State<HelloWorld> {
+class _BasicArGeolocationState extends State<BasicArGeolocation> {
   WidgetDistance situationDistance = WidgetDistance.navigating;
   WidgetCompass situationCompass = WidgetCompass.directing;
 
   late ArCoreController arCoreController;
-  bool anchorWasFound = false;
-  int _clearDirection = 0;
-  double distance = 0;
+  late ArCoreNode fixedNode;
+  late ArCoreNode fixedNode2;
+
+  int _angle = 0;
+  double distanceToFlag = 0;
   int _distance = 0;
-  double targetDegree = 0;
+  double degreeFromUserToFlagPos = 0;
+  double dX = 0;
+  double dY = 0;
+  bool updateNode = false;
+
+  // J.A. innegård
+  //final double _flagPosLat = 57.68862643251456;
+  //final double _flagPosLong = 11.974664533620052;
+
+  // Sandlådan
+  final double _flagPosLat = 57.68850504306074;
+  final double _flagPosLong = 11.979067324351785;
+
+  // Utanför bilbioteket
+  //final double _flagPosLat = 57.69077993841609;
+  //final double _flagPosLong = 11.97887268196273;
   late Timer timer;
 
   //calculation formula of angel between 2 different points
@@ -41,20 +59,8 @@ class _HelloWorldState extends State<HelloWorld> {
 
     brng = vector.degrees(brng);
     brng = (brng + 360) % 360;
-    //brng = 360 - brng; //remove to make clockwise
+    brng = 360 - brng; //remove to make clockwise
     return brng;
-  }
-
-  //device compass
-  void calculateDegree() {
-    FlutterCompass.events!.listen((CompassEvent event) {
-      double? direction = event.heading;
-      setState(() {
-        if (direction != null) {
-          _clearDirection = targetDegree.truncate() - direction.truncate();
-        }
-      });
-    });
   }
 
 //distance between faculty and device coordinates
@@ -83,37 +89,58 @@ class _HelloWorldState extends State<HelloWorld> {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
 
-    final double _facultypositionlat = 57.688668428213035;
-    final double _facultypositionlong = 11.97427593309094;
+    distanceToFlag = Geolocator.distanceBetween(
+        position.latitude, position.longitude, _flagPosLat, _flagPosLong);
 
-    distance = Geolocator.distanceBetween(position.latitude, position.longitude,
-        _facultypositionlat, _facultypositionlong);
+    degreeFromUserToFlagPos = angleFromCoordinate(
+        position.latitude, position.longitude, _flagPosLat, _flagPosLong);
 
-    targetDegree = angleFromCoordinate(position.latitude, position.longitude,
-        _facultypositionlat, _facultypositionlong);
+    dX = (position.latitude - _flagPosLat) * 111000; //sin
+    dY = (position.longitude - _flagPosLong) * 111000; // cos
     calculateDegree();
+  }
+
+  //device compass
+  void calculateDegree() {
+    FlutterCompass.events!.listen((CompassEvent event) {
+      double? deviceDegrees = event.heading;
+      setState(() {
+        if (deviceDegrees != null) {
+          _angle = (deviceDegrees).truncate();
+        }
+      });
+    });
   }
 
   @override
   void initState() {
     super.initState();
     _getlocation(); //first run
-    timer = Timer.periodic(const Duration(seconds: 2), (timer) {
-      _getlocation();
-      if (distance < 50 && distance != 0) {
-        setState(() {
-          situationDistance = WidgetDistance.ready;
-          situationCompass = WidgetCompass.scanning;
-        });
-      } else {
-        setState(() {
-          _distance = distance.truncate();
-          situationDistance = WidgetDistance.navigating;
-          situationCompass = WidgetCompass.directing;
-        });
-      }
-      print("Distance to target: $distance");
-    });
+    timer = Timer.periodic(
+      const Duration(seconds: 2),
+      (timer) {
+        _getlocation();
+        print("dX: " + dX.toString() + " dY: " + dY.toString());
+        if (distanceToFlag < 50 && distanceToFlag != 0) {
+          setState(
+            () {
+              print("Within distance test");
+              situationDistance = WidgetDistance.ready;
+              situationCompass = WidgetCompass.scanning;
+            },
+          );
+        } else {
+          setState(
+            () {
+              print("Outside distance test");
+              _distance = distanceToFlag.truncate();
+              situationDistance = WidgetDistance.navigating;
+              situationCompass = WidgetCompass.directing;
+            },
+          );
+        }
+      },
+    );
   }
 
   @override
@@ -124,92 +151,224 @@ class _HelloWorldState extends State<HelloWorld> {
         title: const Text('Parkrun AR'),
         actions: <Widget>[
           IconButton(
-              icon: const Icon(Icons.help),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) => CustomDialog());
-              })
+            icon: const Icon(Icons.help),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) => CustomDialog());
+            },
+          )
         ],
         flexibleSpace: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: <Color>[
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: <Color>[
                 Color.fromARGB(190, 207, 37, 7),
                 Colors.transparent
-              ])),
+              ],
+            ),
+          ),
         ),
       ),
       body: distanceProvider(),
       floatingActionButton: compassProvider());
 
   Widget readyWidget() {
-    return Container(
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          ArCoreView(
-            onArCoreViewCreated: onArCoreViewCreated,
-          ),
-          anchorWasFound
-              ? Container()
-              : Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(' Distance to flag : $_distance m.',
-                          style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              backgroundColor: Colors.blueGrey,
-                              color: Colors.white)),
-                    ),
-                  ],
-                ),
-        ],
-      ),
+    {
+      print("Yes");
+    }
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        ArCoreView(
+          onArCoreViewCreated: _onArCoreViewCreated,
+          enableTapRecognizer: true,
+        ),
+        Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(' Angle from u to f : $degreeFromUserToFlagPos ',
+                  style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.blueGrey,
+                      color: Colors.white)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(' Device angle : $_angle ',
+                  style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.blueGrey,
+                      color: Colors.white)),
+            ),
+          ],
+        ),
+      ],
     );
   }
+
+  /*void _onPress() {
+    final node = ArCoreReferenceNode(
+      name: "Cube",
+      objectUrl:
+          "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Fox/glTF-Binary/Fox.glb",
+      scale: vector.Vector3(0.2, 0.2, 0.2),
+    );
+    print("Should have added Chicken on target");
+    // Add the node to the AR scene
+    arCoreController.addArCoreNodeWithAnchor(node);
+  }*/
 
   Widget navigateWidget() {
-    return Container(
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          ArCoreView(
-            onArCoreViewCreated: onArCoreViewCreated,
-          ),
-          anchorWasFound
-              ? Container()
-              : Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(' Distance to flag : $_distance m.',
-                          style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              backgroundColor: Colors.blueGrey,
-                              color: Colors.white)),
-                    ),
-                  ],
-                ),
-        ],
-      ),
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        /*ArCoreView(
+          onArCoreViewCreated: (controller) => {
+            arCoreController = controller,
+            print("WTF"),
+          },
+        ),*/
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(' Distance to flag : $_distance m.',
+                  style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.blueGrey,
+                      color: Colors.white)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(' Angle to flag : $degreeFromUserToFlagPos m.',
+                  style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      backgroundColor: Colors.blueGrey,
+                      color: Colors.white)),
+            ),
+          ],
+        ),
+      ],
     );
   }
 
-  void onArCoreViewCreated(ArCoreController arCoreController) {
-    this.arCoreController = arCoreController;
-    //if you want to block AR while you aren't close to target > add "if (situationDistance==WidgetDistance.ready)" here
-    if (situationDistance == WidgetDistance.ready) {
-      _addCylinder(arCoreController);
-      setState(() => anchorWasFound = true);
-    }
+  void _onArCoreViewCreated(ArCoreController controller) {
+    print("Crash 2 test");
+    arCoreController = controller;
+    arCoreController.onNodeTap = (name) => onTapHandler(name);
+    _addNodeOnPlaneDetected();
+  }
+
+  Future<void> _addNodeOnPlaneDetected() async {
+    final moonMaterial = ArCoreMaterial(color: Colors.grey);
+
+    final moonShape = ArCoreSphere(
+      materials: [moonMaterial],
+      radius: distanceToFlag / 60,
+    );
+
+    final moon = ArCoreNode(
+      shape: moonShape,
+      // Vector3(dY, 0, dX)
+      /* . . . - . . .
+         . .   X   . .
+         . . . . . . .
+         - Z . ↑ . Z +
+         . . . . . . .
+         . . . X . . .
+         . . . + . . . 
+         Y kanske är höjd idk*/
+      position: vector.Vector3(-dY, 0.0, dX),
+      rotation: vector.Vector4(0, 0, 0, 0),
+    );
+
+    final earthMaterial = ArCoreMaterial(
+      color: Colors.blue,
+    );
+
+    final earthShape = ArCoreSphere(
+      materials: [earthMaterial],
+      radius: 0.03,
+    );
+
+    final earth = ArCoreNode(
+      shape: earthShape,
+      children: [moon],
+      position: vector.Vector3(0, 0, 0.0),
+      rotation: vector.Vector4(0, 0, 0, 0),
+    );
+
+    await Future.delayed(Duration(seconds: 2));
+
+    arCoreController.addArCoreNodeWithAnchor(earth);
+    updateNode = true;
+  }
+
+  void onTapHandler(String name) {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) =>
+          AlertDialog(content: Text('onNodeTap on $name')),
+    );
+  }
+
+  void _addFlagNodeToWorld() async {
+    print("Crash 1 test");
+    // create a fixed node at the specified latitude and longitude
+    fixedNode = ArCoreReferenceNode(
+      name: 'flag',
+      objectUrl:
+          "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Fox/glTF-Binary/Fox.glb",
+      position: vector.Vector3(16.0, 16.0, -20.0),
+      //vector.Vector3(dX, dY, -distanceToFlag),
+      rotation: vector.Vector4(0, 0, 0, 0), //_calculateRotation(),
+      scale: vector.Vector3(1, 1, 1),
+    );
+
+    /*fixedNode2 = ArCoreNode(
+      name: 'flag2',
+      shape: ArCoreSphere(
+        materials: [
+          ArCoreMaterial(
+            color: Colors.amber,
+          ),
+        ],
+        radius: 1,
+      ),
+      position: vector.Vector3(dX * 1, dY * 1, -distanceToFlag),
+      rotation: _calculateRotation(),
+      scale: vector.Vector3(1, 1, 1),
+    );*/
+
+    print("Crash 3 test");
+    // add the fixed node to the scene
+    // addArCoreNodeWithAnchor ger
+    await Future.delayed(Duration(seconds: 2));
+    //arCoreController.addArCoreNode(fixedNode);
+    arCoreController.addArCoreNodeWithAnchor(fixedNode);
+    //arCoreController.addArCoreNodeWithAnchor(fixedNode2);
+  }
+
+  vector.Vector3 _getFlagPosition() {
+    print("Crash 4 test");
+    return vector.Vector3(dX * 4, dY * 4, distanceToFlag);
+    //return vector.Vector3(3, 3, -1);
+  }
+
+  vector.Vector4 _calculateRotation() {
+    // calculate the rotation based on the user's current heading
+    double rotation = pi * degreeFromUserToFlagPos / 180;
+    return vector.Vector4(0, sin(rotation / 2), 0, cos(rotation / 2));
   }
 
   Widget scanningWidget() {
@@ -234,9 +393,8 @@ class _HelloWorldState extends State<HelloWorld> {
       backgroundColor: Colors.blue,
       onPressed: null,
       child: RotationTransition(
-        turns: AlwaysStoppedAnimation(_clearDirection > 0
-            ? _clearDirection / 360
-            : (_clearDirection + 360) / 360),
+        turns: AlwaysStoppedAnimation(
+            _angle > 0 ? _angle / 360 : (_angle + 360) / 360),
         //if you want you can add animation effect for rotate
         child: Ink(
           decoration: const ShapeDecoration(
@@ -267,26 +425,8 @@ class _HelloWorldState extends State<HelloWorld> {
       case WidgetDistance.ready:
         return readyWidget();
       case WidgetDistance.navigating:
-        anchorWasFound = true;
         return navigateWidget();
     }
-  }
-
-  void _addCylinder(ArCoreController controller) {
-    final material = ArCoreMaterial(
-      color: Colors.red,
-      reflectance: 1.0,
-    );
-    final cylinder = ArCoreCylinder(
-      materials: [material],
-      radius: 3.0,
-      height: 3.0,
-    );
-    final node = ArCoreNode(
-      shape: cylinder,
-    );
-    controller.addArCoreNode(node);
-    print("Loaded cylinder ");
   }
 
   @override
