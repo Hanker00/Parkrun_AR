b1ad10c3a6170c23a84007da0344d8639674968b
Hamid <113691788+HamidHosseini313@users.noreply.github.com>
2023-04-19 16:39:47 +0200

    språk
    ändrat språk för alla section

--- lib/models/map_markers/specific_bandel_marker.dart
+++ lib/models/map_markers/specific_bandel_marker.dart
@@ -59,38 +59,46 @@ class bandel_marks {
   static List<MapMarker> get mapMarker_bandel_2 => _bandel_2;
 
   static final List<MapMarker> _bandel_2 = [
-    KilometerMarker.three(
-        "3km", "", 57.7043708, 12.0472277, "assets/images/section2/3km.jpg"),
+    KilometerMarker.three("3km", "Sign 3 km", 57.7043708, 12.0472277,
+        "assets/images/section2/3km.jpg"),
     DirectionMarker.left(
         "vänster",
-        "Skylt vänster som leder deltagarna upp på 2,5:an - Gröna stigen. Med fördel en skylt innan svängen och en efter.",
+        "Sign to the left that leads the participants up to the 2.5:an - Green Trail. Ideally, there should be one sign before the turn and one after.",
         57.7042189,
         12.0447332,
         "assets/images/section2/flag3.jpg"),
-    DirectionMarker.right("Höger", "Deltagarna ska fortsätta svagt åt höger.",
-        57.703847, 12.0446193, "assets/images/section2/flag2.jpg"),
-    DirectionMarker.right("Höger", "Deltagarna ska fortsätta svagt åt höger.",
-        57.7034586, 12.0447855, "assets/images/section2/flag1.jpg")
+    DirectionMarker.right(
+        "Höger",
+        "The participants should continue slightly to the right",
+        57.703847,
+        12.0446193,
+        "assets/images/section2/flag2.jpg"),
+    DirectionMarker.right(
+        "Höger",
+        "The participants should continue slightly to the right.",
+        57.7034586,
+        12.0447855,
+        "assets/images/section2/flag1.jpg")
   ];
 
   static List<MapMarker> get mapMarker_bandel_3 => _bandel_3;
 
   static final List<MapMarker> _bandel_3 = [
-    DirectionMarker("Rakt fram 3", "Vid husknuten.", 57.7030581, 12.0370728,
-        "assets/images/section3/f1.jpg"),
-    DirectionMarker("Rakt fram", "I backen efter korsningen.", 57.6995955,
-        12.0370352, "assets/images/section3/f2.jpg"),
-    KilometerMarker.four("4 km", "4 km. ", 57.6994292, 12.0395458,
+    DirectionMarker("Rakt fram 3", "At the corner of the house", 57.7030581,
+        12.0370728, "assets/images/section3/f1.jpg"),
+    DirectionMarker("Rakt fram", "In the hill after the intersection..",
+        57.6995955, 12.0370352, "assets/images/section3/f2.jpg"),
+    KilometerMarker.four("4 km", "Sign 4 km. ", 57.6994292, 12.0395458,
         "assets/images/section3/4km.jpg"),
     DirectionMarker(
         "Rakt fram",
-        "skylt rakt fram strax efter korsningen av åttan.",
+        "Sign straight ahead just after the intersection of the number eight trail.",
         57.7003114,
         12.0401587,
         "assets/images/section3/f3.jpg"),
     DirectionMarker.right(
         "Höger",
-        "Skylt höger för att deltagarna inte ska ta genvägen.",
+        "Sign to the right so that the participants do not take the shortcut.",
         57.7014113,
         12.0432821,
         "assets/images/section3/f4.jpg"),
