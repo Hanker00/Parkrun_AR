3269ad09fbe93e1c4b8d8b3b3e0e03e049a7dc6f
Hamid <113691788+HamidHosseini313@users.noreply.github.com>
2023-04-25 15:48:13 +0200

    uppdaterat from main

--- lib/widgets/current_step.dart
+++ lib/widgets/current_step.dart
@@ -4,8 +4,6 @@ import 'package:parkrun_ar/models/themeData/theme.dart';
 import 'package:provider/provider.dart';
 
 class CurrentStep extends StatefulWidget {
-  // takes in a marker and also the total amount of markers
-
   const CurrentStep({super.key});
 
   @override
@@ -16,85 +14,111 @@ class _CurrentStepState extends State<CurrentStep> {
   @override
   Widget build(BuildContext context) {
     final notifierState = context.watch<StateNotifierInstruction>();
-    return Column(children: [
-      Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          OutlinedButton(
+    final currentMarker = notifierState.notifierMarker[notifierState.counter];
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            OutlinedButton(
               onPressed: () => notifierState.goBack(),
-              child: const Text("go back")),
-          const Text(
-            "Current Sign",
-            style: TextStyle(
-              fontSize: 24,
+              child: const Text("go back"),
             ),
-          ),
-          OutlinedButton(
+            const Text(
+              "Current Sign",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            OutlinedButton(
               onPressed: () => notifierState.increment(),
-              child: const Text("next Sign")),
-        ],
-      ),
-
-      Row(
-        //icon||description | 1/8
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                gradient: const LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [colorPrimaryLight, colorPrimary],
+              child: const Text("next Sign"),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  gradient: const LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [colorPrimaryLight, colorPrimary],
+                  ),
+                  borderRadius: BorderRadius.circular(3),
                 ),
-                borderRadius: BorderRadius.circular(3),
-              ),
-              child: Icon(
-                notifierState.notifierMarker[notifierState.counter].markerIcon,
-                size: 50,
-                color: Colors.white,
+                child: Icon(
+                  notifierState
+                      .notifierMarker[notifierState.counter].markerIcon,
+                  size: 50,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-
-          //title of current sign and the description
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(notifierState.notifierMarker[notifierState.counter].title,
-                    style: Theme.of(context).textTheme.displayMedium),
-                Text(
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    notifierState.notifierMarker[notifierState.counter].title,
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  Text(
                     notifierState
                         .notifierMarker[notifierState.counter].description,
                     overflow: TextOverflow.clip,
-                    style: Theme.of(context).textTheme.bodySmall),
-              ],
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
             ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              '${notifierState.counter + 1} / ${notifierState.notifierMarker.length.toString()} ',
-              style: const TextStyle(
-                  fontSize: 24, color: Color.fromRGBO(137, 137, 137, 100)),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                '${notifierState.counter + 1} / ${notifierState.notifierMarker.length.toString()} ',
+                style: const TextStyle(
+                  fontSize: 24,
+                  color: Color.fromRGBO(137, 137, 137, 100),
+                ),
+              ),
             ),
-          ),
-        ],
-      ),
-
-      // Buttons with AR and show pictures, inactive for now
-      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        TextButton(onPressed: () => null, child: const Text("Show photo")),
-        ElevatedButton(
-          onPressed: () => null,
-          child: const Text("use AR"),
+          ],
         ),
-      ])
-    ]);
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) => AlertDialog(
+                    title: const Text('Marker Photo'),
+                    content: Image.asset(
+                      currentMarker.imagePath,
+                      fit: BoxFit.cover,
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text('Close'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: const Text('Show photo'),
+            ),
+            ElevatedButton(
+              onPressed: () => null,
+              child: const Text('use AR'),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
