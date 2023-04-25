407ad29355f4fce970d5bd942bed98dcf2f7b98a
Hamid <113691788+HamidHosseini313@users.noreply.github.com>
2023-04-25 23:37:07 +0200

    uppdaterat
    1-  gjorde om  current step som kan visa bilden
    2- gjorde om specific marker

--- lib/models/map_markers/specific_bandel_marker.dart
+++ lib/models/map_markers/specific_bandel_marker.dart
@@ -12,42 +12,42 @@ class BandelMarks {
   ];
 
   static final List<MapMarker> _bandel_1 = [
-    DirectionMarker("Rakt fram 3", "Skylt rakt. ", 57.70631, 12.04014,
+    DirectionMarker(
+        "Rakt fram",
+        "Skylt rakt fram strax efter fyrvägskorsningen",
+        57.70631,
+        12.04014,
         "assets/images/section1/sign1.jpg"),
     DirectionMarker.right(
-        "Höger 4",
-        "Skylt vid stenen som leder deltagarna.\ntill höger, fortsatt på åttan.",
+        "Höger",
+        "Skylt vid stenen som leder deltagarna till höger, fortsatt på åttan.",
         57.70743,
         12.03822,
         "assets/images/section1/sign2.jpg"),
     KilometerMarker.one(
-        "1 km id:9",
+        "1 km",
         "Skylt 1 km under den sista högspänningsledningen.",
         57.70771,
         12.03938,
         "assets/images/section1/1km.jpg"),
     KilometerMarker.two(
-        "2 km id:10",
+        "2 km",
         "Skylt 2 km vid klippväggen på vänster sida, strax innan staketet börjar.",
         57.71038,
         12.05371,
         "assets/images/section1/2km.jpg"),
-    DirectionMarker("Rakt fram 5", "Skylt rakt fram i höjd med staketet.",
-        57.7103, 12.05403, "assets/images/section1/sign3.jpg"),
+    DirectionMarker("Rakt fram", "Skylt rakt fram i höjd med staketet", 57.7103,
+        12.05403, "assets/images/section1/sign4.jpg"),
+    DirectionMarker.right("Höger", "Skylt mot höger, framför brun fast skylt.",
+        57.71056, 12.05433, "assets/images/section1/sign4.jpg"),
     DirectionMarker.right(
-        "Höger id:6",
-        "Skylt mot höger, framför brun fast skylt.",
-        57.71056,
-        12.05433,
-        "assets/images/section1/sign4.jpg"),
-    DirectionMarker.right(
-        "Höger id:7",
+        "Höger",
         "Skylt höger som leder deltagarna upp på Ormeslättsstigen, bakom Servicehuset.",
         57.71046,
         12.05523,
         "assets/images/section1/sign5.jpg"),
     DirectionMarker.right(
-        "Höger id:8",
+        "Höger",
         "Skylt höger som leder deltagarna vidare på Ormeslättsstigen, strax efter en liten backe.",
         57.70652,
         12.05289,
@@ -56,35 +56,35 @@ class BandelMarks {
 
   static final List<MapMarker> _bandel_2 = [
     KilometerMarker.three(
-        "3km", "", 57.7043708, 12.0472277, "assets/images/section2/3km.jpg"),
+        "3 km", "", 57.7043708, 12.0472277, "assets/images/section2/3km.jpg"),
     DirectionMarker.left(
-        "vänster",
+        "Vänster",
         "Skylt vänster som leder deltagarna upp på 2,5:an - Gröna stigen. Med fördel en skylt innan svängen och en efter.",
         57.7042189,
         12.0447332,
         "assets/images/section2/flag3.jpg"),
-    DirectionMarker.right("Höger", "Deltagarna ska fortsätta svagt åt höger.",
+    DirectionMarker.right("Höger", "Deltagarna ska fortsätta svagt åt höger",
         57.703847, 12.0446193, "assets/images/section2/flag2.jpg"),
-    DirectionMarker.right("Höger", "Deltagarna ska fortsätta svagt åt höger.",
+    DirectionMarker.right("Höger", "Deltagarna ska fortsätta svagt åt höger",
         57.7034586, 12.0447855, "assets/images/section2/flag1.jpg")
   ];
 
   static final List<MapMarker> _bandel_3 = [
-    DirectionMarker("Rakt fram 3", "Vid husknuten.", 57.7030581, 12.0370728,
+    DirectionMarker("Rakt fram", "Vid husknuten", 57.7030581, 12.0370728,
         "assets/images/section3/f1.jpg"),
-    DirectionMarker("Rakt fram", "I backen efter korsningen.", 57.6995955,
+    DirectionMarker("Rakt fram", "I backen efter korsningen", 57.6995955,
         12.0370352, "assets/images/section3/f2.jpg"),
-    KilometerMarker.four("4 km", "4 km. ", 57.6994292, 12.0395458,
-        "assets/images/section3/4km.jpg"),
+    KilometerMarker.four(
+        "4 km", "", 57.6994292, 12.0395458, "assets/images/section3/4km.jpg"),
     DirectionMarker(
         "Rakt fram",
-        "skylt rakt fram strax efter korsningen av åttan.",
+        "skylt rakt fram strax efter korsningen av åttan",
         57.7003114,
         12.0401587,
         "assets/images/section3/f3.jpg"),
     DirectionMarker.right(
         "Höger",
-        "Skylt höger för att deltagarna inte ska ta genvägen.",
+        "Skylt höger för att deltagarna inte ska ta genvägen",
         57.7014113,
         12.0432821,
         "assets/images/section3/f4.jpg"),
@@ -92,19 +92,15 @@ class BandelMarks {
 
   static final List<MapMarker> _hubbenTest = [
     DirectionMarker("Hubben test1", "Fin skylt :) :)", 57.68846, 11.97912,
-        "assets/icons/marker.png)"),
+        "assets/icons/markers"),
     DirectionMarker.right("Hubben test2", "Fin skylt :) :)", 57.68961, 11.97833,
-        "assets(icons/marker.png"),
+        "assets/icons/markers"),
     DirectionMarker.left("Hubben test3", "Fin skylt :) :) :)", 57.68963,
-        11.97708, "assets(icons/marker.png"),
+        11.97708, "assets/icons/markers"),
     DirectionMarker.uTurn(
-      "Hubben test4",
-      "Fin skylt :) :) :) :)",
-      57.68901,
-      11.97697,
-    ),
+        "Hubben test4", "Fin skylt :) :) :) :)", 57.68901, 11.97697),
     DirectionMarker("Hubben test1", "Fin skylt :) :) :) :) :)", 57.68819,
-        11.97831, "assets/icons/marker.png"),
+        11.97831, "assets/icons/markers"),
   ];
 
   static List<MapMarker> get hubbenMarker => _hubbenTest;
--- lib/widgets/current_step.dart
+++ lib/widgets/current_step.dart
@@ -4,6 +4,8 @@ import 'package:parkrun_ar/models/themeData/theme.dart';
 import 'package:provider/provider.dart';
 
 class CurrentStep extends StatefulWidget {
+  // takes in a marker and also the total amount of markers
+
   const CurrentStep({super.key});
 
   @override
@@ -15,110 +17,108 @@ class _CurrentStepState extends State<CurrentStep> {
   Widget build(BuildContext context) {
     final notifierState = context.watch<StateNotifierInstruction>();
     final currentMarker = notifierState.notifierMarker[notifierState.counter];
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            OutlinedButton(
+    return Column(children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          OutlinedButton(
               onPressed: () => notifierState.goBack(),
-              child: const Text("go back"),
+              child: const Text("go back")),
+          const Text(
+            "Current Sign",
+            style: TextStyle(
+              fontSize: 24,
             ),
-            const Text(
-              "Current Sign",
-              style: TextStyle(
-                fontSize: 24,
-              ),
-            ),
-            OutlinedButton(
+          ),
+          OutlinedButton(
               onPressed: () => notifierState.increment(),
-              child: const Text("next Sign"),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  gradient: const LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [colorPrimaryLight, colorPrimary],
-                  ),
-                  borderRadius: BorderRadius.circular(3),
+              child: const Text("next Sign")),
+        ],
+      ),
+
+      Row(
+        //icon||description | 1/8
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                gradient: const LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [colorPrimaryLight, colorPrimary],
                 ),
-                child: Icon(
-                  notifierState
-                      .notifierMarker[notifierState.counter].markerIcon,
-                  size: 50,
-                  color: Colors.white,
-                ),
+                borderRadius: BorderRadius.circular(3),
+              ),
+              child: Icon(
+                notifierState.notifierMarker[notifierState.counter].markerIcon,
+                size: 50,
+                color: Colors.white,
               ),
             ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    notifierState.notifierMarker[notifierState.counter].title,
-                    style: Theme.of(context).textTheme.displayMedium,
-                  ),
-                  Text(
+          ),
+
+          //title of current sign and the description
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(notifierState.notifierMarker[notifierState.counter].title,
+                    style: Theme.of(context).textTheme.displayMedium),
+                Text(
                     notifierState
                         .notifierMarker[notifierState.counter].description,
                     overflow: TextOverflow.clip,
-                    style: Theme.of(context).textTheme.bodySmall,
+                    style: Theme.of(context).textTheme.bodySmall),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              '${notifierState.counter + 1} / ${notifierState.notifierMarker.length.toString()} ',
+              style: const TextStyle(
+                  fontSize: 24, color: Color.fromRGBO(137, 137, 137, 100)),
+            ),
+          ),
+        ],
+      ),
+
+      // Buttons with AR inactive for now
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('Marker Photo'),
+                  content: Image.asset(
+                    currentMarker.imagePath,
+                    fit: BoxFit.cover,
                   ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Text(
-                '${notifierState.counter + 1} / ${notifierState.notifierMarker.length.toString()} ',
-                style: const TextStyle(
-                  fontSize: 24,
-                  color: Color.fromRGBO(137, 137, 137, 100),
-                ),
-              ),
-            ),
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextButton(
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) => AlertDialog(
-                    title: const Text('Marker Photo'),
-                    content: Image.asset(
-                      currentMarker.imagePath,
-                      fit: BoxFit.cover,
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text('Close'),
                     ),
-                    actions: [
-                      TextButton(
-                        onPressed: () => Navigator.pop(context),
-                        child: const Text('Close'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-              child: const Text('Show photo'),
-            ),
-            ElevatedButton(
-              onPressed: () => null,
-              child: const Text('use AR'),
-            ),
-          ],
-        ),
-      ],
-    );
+                  ],
+                ),
+              );
+            },
+            child: const Text('Show photo'),
+          ),
+          ElevatedButton(
+            onPressed: () => null,
+            child: const Text('use AR'),
+          ),
+        ],
+      ),
+    ]);
   }
 }
