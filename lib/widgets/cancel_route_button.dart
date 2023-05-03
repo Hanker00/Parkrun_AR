26c484ca0ee2fab4f869f9d8c5b921a069f6e0fd
ClaraSimonsson <clara.simonsson@hotmail.se>
2023-05-03 15:01:16 +0200

    feat: Styling of cancel button and cleaned up/fixed the routing between screens

--- lib/widgets/cancel_route_button.dart
+++ lib/widgets/cancel_route_button.dart
@@ -6,19 +6,23 @@ class CancelRouteButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        _onPressed(context);
-      },
-      style: Theme.of(context).elevatedButtonTheme.style,
-      child: Text("Cancel navigation",
-          style: Theme.of(context).textTheme.headlineSmall),
-    );
+    return Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ElevatedButton(
+            onPressed: () {
+              _onPressed(context);
+            },
+            style: Theme.of(context).elevatedButtonTheme.style,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Cancel navigation",
+                  style: Theme.of(context).textTheme.headlineSmall),
+            )));
   }
 
   void _onPressed(BuildContext context) {
-    // 1: Cancel everything related to the navigation itself
-    // 2: Redirect back to the starting page
-    Navigator.of(context).pushNamed(Navigator.defaultRouteName);
+    // Remove all screens until only the homepage exist
+    Navigator.of(context).pushNamedAndRemoveUntil(
+        Navigator.defaultRouteName, ModalRoute.withName('/'));
   }
 }
