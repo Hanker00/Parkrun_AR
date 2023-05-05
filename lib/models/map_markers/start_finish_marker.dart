d735207401b074e48dafe41f4edbe8b6aa8c999e
Axel Söderberg <karlaxel01@hotmail.com>
2023-05-05 19:40:18 +0200

    long time since main was merge into branch

--- lib/models/map_markers/start_finish_marker.dart
+++ lib/models/map_markers/start_finish_marker.dart
@@ -6,5 +6,13 @@ import 'map_marker.dart';
 /// Subclass of [MapMarker] creates a Start and Finish Marker that contains an icon with a checkered flag.
 class StartFinishMarker extends MapMarker {
   /// Creates a Start and Finish Marker that contains an icon with a checkered flag.
-  StartFinishMarker(title, String description, double startLatitude, double startLongitude) : super(markerIcon: Icons.sports_score, title:title, description:description, startLatitude: startLatitude, startLongitude: startLongitude);
+  StartFinishMarker(
+      title, String description, double startLatitude, double startLongitude)
+      : super(
+            markerIcon: Icons.sports_score,
+            title: title,
+            description: description,
+            startLatitude: startLatitude,
+            startLongitude: startLongitude,
+            imagePath: '');
 }
